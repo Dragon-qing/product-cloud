eceb264be027a0665ef07870d062abdef284ee8b
Aprilier <2558739764@qq.com>
2022-07-08 11:24:02 +0800

    person

---
+++ src/views/personal/index.vue
@@ -0,0 +1,159 @@
+<template>
+  <a-layout>
+    <a-layout-header class="header">
+      <div class="logo" />
+      <a-menu v-model="selectedKeys1" theme="dark" mode="horizontal" :style="{ lineHeight: '64px' }">
+        <a-menu-item key="1">
+          <a-avatar size="large">
+            <template #icon><UserOutlined /></template>
+          </a-avatar>
+          用户
+        </a-menu-item>
+      </a-menu>
+    </a-layout-header>
+    <a-layout>
+      <a-layout-sider width="200" style="background: #fff">
+        <a-menu v-model="selectedKeys2" mode="inline" :style="{ height: '100%', borderRight: 0 }">
+          <a-sub-menu key="sub1">
+            <template #title>
+              <span> <user-outlined />个人信息 </span>
+            </template>
+            <a-menu-item key="1">个人简介</a-menu-item>
+            <a-menu-item key="2">成果展示</a-menu-item>
+          </a-sub-menu>
+          <a-sub-menu key="sub2">
+            <template #title>
+              <span>
+                <laptop-outlined />
+                产品与分类树
+              </span>
+            </template>
+            <a-menu-item key="3">我的产品</a-menu-item>
+            <a-menu-item key="4">我的分类树</a-menu-item>
+          </a-sub-menu>
+          <a-sub-menu key="sub3">
+            <template #title>
+              <span>
+                <notification-outlined />
+                照片与视频
+              </span>
+            </template>
+            <a-menu-item key="5">照片</a-menu-item>
+            <a-menu-item key="6">视频</a-menu-item>
+          </a-sub-menu>
+        </a-menu>
+      </a-layout-sider>
+      <a-layout style="padding: 0 24px 24px">
+        <a-breadcrumb style="margin: 16px 0">
+          <a-breadcrumb-item>welcome</a-breadcrumb-item>
+        </a-breadcrumb>
+        <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+          <!-- <a-card title="于晓波 yuxbb@digitalchina.com" style="width: 1230px">
+            <p>个人简介</p>
+            <p>个人简介</p>
+            <p>个人简介</p>
+          </a-card> -->
+          <!-- <a-empty /> -->
+          <!-- <a-card title="HR数字化" style="width: 1230px">
+            <p>分类树链接： https://yuntu.digitalchina.com/classfication?tree_id=7456</p>
+            <p>产品： 244 浏览： 4305</p>
+            <p>最近浏览：</p>
+          </a-card>
+          <a-card title="DCIT产品工具树" style="width: 1230px">
+            <p>分类树链接： https://yuntu.digitalchina.com/classfication?tree_id=9186</p>
+            <p>产品： 43 浏览： 241</p>
+            <p>最近浏览：</p>
+          </a-card> -->
+          <!-- <a-descriptions title="北森iTalentX" bordered>
+            <a-descriptions-item label="产品名称">北森iTalentX</a-descriptions-item>
+            <a-descriptions-item label="解决方案"> 解决方案 DHR一体化解决方案</a-descriptions-item>
+            <a-descriptions-item label="官网地址">https://www.beisen.com/</a-descriptions-item>
+            <a-descriptions-item label="公司名称">北京北森云计算股份有限公司</a-descriptions-item>
+            <a-descriptions-item label="创始人" :span="2">王朝晖</a-descriptions-item>
+            <a-descriptions-item label="公司简介" :span="3">
+              北森成立于2005年 [1] ，人力资源科技公司，拥有国内一体化HR SaaS和人才管理平台——iTalentX，
+              为客户提供云端HR软件、人才管理技术和平台的端到端整体解决方案。
+            </a-descriptions-item>
+            <a-descriptions-item label="产品简介" :span="3">
+              北森以人才管理技术为内核，以核心人力为基础， 将人才管理的各个模块紧密集成在一起，形成一体化的HR
+              SaaS及人才管理平台——iTalentX，包括： l People Science：
+              能力模型、人才测评、360评估、考试、调查等人才解决方案。 l
+              核心人力：基础人事档案、薪酬、请假和考勤、员工服务等基础人力资源软件。 l
+              人才管理：招聘、入职、目标和绩效、学习、盘点和继任等人才管理软件。 l
+              员工服务生态：iTalentX连接衣食住行各类互联网服务，实现员工服务互联网化。 l
+              平台和数据：构建于统一PaaS平台的HR SaaS软件，可以实现全平台、跨应用的数据一体化。
+              iTalentX可以帮助企业实现从员工招募、入职、管理到离职的全面数字化管理，快速提升人才管理能力，
+              提升人力资源管理效率，帮助员工快速成长，实现智慧决策。
+            </a-descriptions-item>
+            <a-descriptions-item label="产品详情">
+              产品分类：人力资源>HR系统
+              <br />
+              总体评价：4.3 产品功能：4.3
+              <br />
+              用户体验：4.7 交付服务：4.3
+              <br />
+              产品价格：0.0 成本商务：4.7
+              <br />
+              价格生态：4.3
+              <br />
+              用户标签：商用
+              <br />
+            </a-descriptions-item>
+          </a-descriptions> -->
+        </a-layout-content>
+      </a-layout>
+    </a-layout>
+  </a-layout>
+</template>
+<script>
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons-vue'
+import { defineComponent, ref } from 'vue'
+// const data = [
+//   {
+//     title: 'Title 1',
+//   },
+//   {
+//     title: 'Title 2',
+//   },
+//   {
+//     title: 'Title 3',
+//   },
+//   {
+//     title: 'Title 4',
+//   },
+// ]
+export default defineComponent({
+  components: {
+    UserOutlined,
+    LaptopOutlined,
+    NotificationOutlined,
+  },
+  setup() {
+    return {
+      selectedKeys1: ref(['2']),
+      selectedKeys2: ref(['1']),
+      // data,
+      collapsed: ref(false),
+      openKeys: ref(['sub1']),
+    }
+  },
+})
+</script>
+<style>
+#components-layout-demo-top-side-2 .logo {
+  float: left;
+  width: 120px;
+  height: 31px;
+  margin: 16px 24px 16px 0;
+  background: rgba(255, 255, 255, 0.3);
+}
+
+.ant-row-rtl #components-layout-demo-top-side-2 .logo {
+  float: right;
+  margin: 16px 0 16px 24px;
+}
+
+.site-layout-background {
+  background: #fff;
+}
+</style>
