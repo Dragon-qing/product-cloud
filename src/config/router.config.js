7af1c1440820fb489ac45fc9a95ed488b5407565
Aprilier <2558739764@qq.com>
2022-07-10 11:06:05 +0800

    classification

--- src/config/router.config.js
+++ src/config/router.config.js
@@ -315,4 +315,27 @@ export const constantRouterMap = [
       },
     ],
   },
+
+  {
+    path: '/classification',
+    name: 'classification',
+    component: BlankLayout,
+    children: [
+      {
+        path: '/classification/tree',
+        name: 'tree',
+        component: () => import('@/views/classification/tree'),
+      },
+      {
+        path: '/classification/nextTree',
+        name: 'nextTree',
+        component: () => import('@/views/classification/nextTree'),
+      },
+      {
+        path: '/classification/project',
+        name: 'project',
+        component: () => import('@/views/classification/project'),
+      },
+    ],
+  },
 ]
