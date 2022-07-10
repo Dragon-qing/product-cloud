0c939a81eec3a970bfdbbde37b37e9d014ab9ebc
万迈洵 <1259337898@qq.com>
2022-07-10 12:16:20 +0800

    7.10

---
+++ src/views/personal/ClassificationTree.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <a-card title="HR数字化" style="">
+      <p>分类树链接： https://yuntu.digitalchina.com/classfication?tree_id=7456</p>
+      <p>产品： 244 浏览： 4305</p>
+      <p>最近浏览：</p>
+    </a-card>
+    <a-card title="DCIT产品工具树" style="">
+      <p>分类树链接： https://yuntu.digitalchina.com/classfication?tree_id=9186</p>
+      <p>产品： 43 浏览： 241</p>
+      <p>最近浏览：</p>
+    </a-card>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/personal/introduction.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <a-card title="于晓波 yuxbb@digitalchina.com" style="width: 1230px">
+      <p>个人简介</p>
+      <p>个人简介</p>
+      <p>个人简介</p>
+    </a-card>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+</style>
